3e5bee3fc4ddcee3c3ffcbcdf59db4a1c716b099
Joseprieto2019 <joseprieto2019@gmx.es->
2019-01-29 11:37:41 -0300

    rmejorando

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ var path = __dirname + '/views/';
 var customers = [];
 
 router.use(function (req,res,next) {
-  console.log("/mmmmmmmmmmmmmmmm" + req.method);
+  //console.log("/mmmmmmmmmmmmmmmm" + req.method);
   next();
 });
 
 app.get("/",function(req,res){
-  console.log("/xxxxxxxxxxxxxx" + req.method);
+  //console.log("/xxxxxxxxxxxxxx" + req.method);
   res.sendFile(path + "index.html");
 });
 
@@ -40,7 +40,6 @@ app.post("/api/customers/save", function(req,res){
 });
 
 app.get("/api/customers/all", function(req,res){
-	console.log("Get All Customers");
 	let customers = [];
 	let customer = {};
 	customer.firstname = Math.round(1000+Math.random()*1000);
@@ -48,16 +47,13 @@ app.get("/api/customers/all", function(req,res){
 	
 	var getJSON = require('get-json')
 	getJSON('https://v20.lvis.io/feeds/latest_results/7/72d0ab6d-3c52-413f-871e-fb47f8aef162.json', function(error, response){
-				h = JSON.stringify(response );
-				let n
-				if(h!=undefined)
-				{
-					let n = h.indexOf('rating',1)
-					let resu = h.substring( n+8 , n+13);
-					console.log(resu + customer.lastname)
-					customer.lastname = resu;
-					customers.push(customer);
-					return res.send(customers);
+
+		var c=response.elements[0].rating;		
+		if(c!=undefined)
+		{
+			customer.lastname = c;
+			customers.push(customer);
+			return res.send(customers);
 			}
 			 else{let n =0}
 			 
@@ -77,7 +73,7 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html"); 
 });
 
-app.set('port', process.env.PORT || 3000); 
+app.set('port', process.env.PORT || 80); 
 app.listen(app.get('port'), () => {
   console.log(`server on port ${app.get('port')}`);
 }); 
